3ee8d918d6c725cc9c59d461b0d7e58180c97ec0
nlok5923 <nlokhande5923@gmail.com>
2021-06-24 12:07:13 +0530

    using monaco editor

--- src/pages/IDE/index.jsx
+++ src/pages/IDE/index.jsx
@@ -1,10 +1,6 @@
 import { React, useState, useEffect } from "react";
 import "./IDE.scss";
-import Editor from 'react-simple-code-editor';
-import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-// import 'prismjs/components/prism-markup';
+import MonacoEditor from 'react-monaco-editor';
 
 const IDE = () => {
 
@@ -12,35 +8,40 @@ const IDE = () => {
         `function add(a, b) {\n  return a + b;\n}`
       );
 
+      const options = {
+        selectOnLineNumbers: true,
+        colorDecorators: true,
+        cursorStyle: "line-thin",
+        folding: true,
+        foldingHighlight: true
+      }; 
+
   return (
     <div>
-      <div className="header">
-        <div className="header-buttons">
-          <button className="header-buttons-run"> Run </button>
-          <select className="header-buttons-choose">
-            <option className="header-buttons-option" value="Python">
-              {" "}
-              Python{" "}
+      <div className="nav">
+        <div className="nav-buttons">
+          <button className="nav-buttons-run"> Run </button>
+          <select className="nav-buttons-choose">
+            <option className="nav-buttons-option" value="Python">
+              Python
             </option>
-            <option className="header-buttons-option" value="Java">
-              {" "}
-              Java{" "}
+            <option className="nav-buttons-option" value="Java">
+              Java
             </option>
-            <option className="header-buttons-option" value="C++">
-              {" "}
-              C++{" "}
+            <option className="nav-buttons-option" value="C++">
+              C++
             </option>
           </select>
         </div>
-        <div className="header-logo">
+        <div className="nav-logo">
           MUSICAL IDE
           <img
-            className="header-logo-img"
+            className="nav-logo-img"
             src="asset/images/logo.svg"
             alt="logo"
           />
         </div>
-        <div className="header-music-player">
+        <div className="nav-music-player">
           <button>test</button>
         </div>
       </div>
@@ -52,16 +53,39 @@ const IDE = () => {
           backgroundPosition: "center",
         }} 
       >
-    <Editor
+
+{/* constructor(props) {
+    super(props);
+    this.state = {
+      code: '// type your code...',
+    }
+  }
+  editorDidMount(editor, monaco) {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  onChange(newValue, e) {
+    console.log('onChange', newValue, e);
+  }
+  render() {
+    const code = this.state.code;
+    const options = {
+      selectOnLineNumbers: true
+    }; */}
+    {/* // return ( */}
+      <MonacoEditor
+        width="100%"
+        height="500"
+        language="cpp"
+        theme="vs"
         value={code}
-        onValueChange={(code) => setCode(code)}
-        highlight={(code) => highlight(code, languages.jsx)}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
-        }}
+        options={options}
+        // onChange={::this.onChange}
+        // editorDidMount={::this.editorDidMount}
       />
+    {/* // ); */}
+  {/* // } */}
+
       </div>
       <div className="i-o">
         <div className="i-o-input">
