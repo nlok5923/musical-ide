24d2706ac95844867dbb40dccb8ab95ca9f4a7f1
nlok5923 <nlokhande5923@gmail.com>
2021-06-30 23:02:05 +0530

    added judge0 compiler

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,64 +1,31 @@
 import { React, useState, useEffect } from "react";
 import "./Player.scss";
-import Player from "./Equipments/Player";
-import { Draggable } from "react-draggable";
+import ReactJkMusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";
 
 const MusicPlayer = () => {
-  // const [songs] = useState([
-  //     {
-  // title: "Forget me too ft. Halsey",
-  // artist: "Machine Gun Kelly",
-  // img_src: "./asset/images/song-1.jpg",
-  // src: "./asset/tune/on-n-on.mp3",
-  //       genre: "test1"
-  //     },
-  //     {
-  // title: "Song 2",
-  // artist: "Artist 2",
-  // img_src: "./asset/images/song-2.jpg",
-  // src: "./asset/tune/somebody-new.mp3",
-  //       genre: "test1"
-  //     },
-  //     {
-  // title: "Song 3",
-  // artist: "Artist 3",
-  // img_src: "./asset/images/song-3.jpg",
-  // src: "./asset/tune/wore.mp3",
-  // genre: "test2"
-  //     },
-  //     {
-  //       title: "Song 4",
-  //       artist: "Artist 4",
-  //       img_src: "./asset/images/song-4.jpg",
-  //       src: "./asset/tune/beleiver.mp3",
-  //       genre: "test2"
-  //     },
-  //     {
-  //         title: "Song 4",
-  //         artist: "Artist 4",
-  //         img_src: "./asset/images/song-1.jpg",
-  //         src: "./asset/tune/tun.mp3",
-  //         genre: "test2"
-  //       },
-  //       {
-  // title: "Song 1",
-  // artist: "Artist 4",
-  // img_src: "./asset/images/song-1.jpg",
-  // src: "./asset/tune/anymore.mp3",
-  // genre: "test2"
-  //       }
-  //   ]);
+  const audioLists = [
+    {
+      name: "Despacito",
+      singer: "Luis Fonsi",
+      cover: "./asset/images/song-1.jpg",
+      musicSrc: "http://localhost:5000/track/60dc0b1d4239615608620f67?",
+    },
+    {
+      name: "Despacito",
+      singer: "Luis Fonsi",
+      cover: "./asset/images/song-3.jpg",
+      musicSrc: "http://localhost:5000/track/60dc0b1d4239615608620f67?",
+    },
+  ];
 
-  return (
-    <div className="App">
-      <Player
-        // currentSongIndex={currentSongIndex}
-        // setCurrentSongIndex={setCurrentSongIndex}
-        // nextSongIndex={nextSongIndex}
-        // songs={songs}
+  return (<div 
+  className="App">
+     <ReactJkMusicPlayer
+        audioLists={audioLists}
+        autoPlay={true}
       />
-    </div>
-  );
+  </div>);
 };
 
 export default MusicPlayer;
--- src/pages/IDE/index.jsx
+++ src/pages/IDE/index.jsx
@@ -3,10 +3,12 @@ import "./IDE.scss";
 import MonacoEditor from "react-monaco-editor";
 import Player from "../../components/Player/index";
 import Draggable from "react-draggable";
+import axios from "axios";
+import base64 from "base-64"
 
 const IDE = () => {
-  const [code] = useState(`function add(a, b) {\n  return a + b;\n}`);
-
+  const [code , setCode] = useState(`#include<bits/stdc++.h> using namespace std; int main() {\n  cout<<"hell0"<<endl; return 0; \n}`);
+  const [input, setInput] = useState("test");
   const options = {
     selectOnLineNumbers: true,
     colorDecorators: true,
@@ -15,11 +17,110 @@ const IDE = () => {
     foldingHighlight: true,
   };
 
+const handleSubmit = async () => {
+  console.log(btoa(code));
+  console.log(atob("I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbih2b2lkKSB7CiAgY2hhciBuYW1lWzEwXTsKICBzY2FuZigiJXMiLCBuYW1lKTsKICBwcmludGYoImhlbGxvLCAlc1xuIiwgbmFtZSk7CiAgcmV0dXJuIDA7Cn0="))
+  console.log(base64.encode(code))
+  var options = {
+    method: 'POST',
+    url: 'https://judge0-ce.p.rapidapi.com/submissions',
+    params: {base64_encoded: 'true', fields: '*'},
+    headers: {
+      'content-type': 'application/json',
+      'x-rapidapi-key': 'acbeeacb85mshceacaa6063ca021p1f634bjsn6775c6acac73',
+      'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+    },
+    data:{
+       language_id : 52,
+       source_code: "I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbih2b2lkKSB7CiAgY2hhciBuYW1lWzEwXTsKICBzY2FuZigiJXMiLCBuYW1lKTsKICBwcmludGYoImhlbGxvLCAlc1xuIiwgbmFtZSk7CiAgcmV0dXJuIDA7Cn0=",
+       stdin: "SnVkZ2Uw"
+  }
+  };
+
+  let token = "";
+  axios.request(options).then(function (response) {
+    // console.log(response.data);
+    token = response.data.token;
+  }).then(() => {
+    var optionSubmission = {
+      method: 'GET',
+      url: `https://judge0-ce.p.rapidapi.com/submissions/${token}?base64_encoded=true`,
+      params: {base64_encoded: 'true', fields: '*'},
+      headers: {
+        'x-rapidapi-key': 'acbeeacb85mshceacaa6063ca021p1f634bjsn6775c6acac73',
+        'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+      }
+    };
+    
+    axios.request(optionSubmission).then(function (resp) {
+      console.log(resp.data);
+      console.log(atob(resp.data.stdout));
+    }).catch(function (error) {
+      console.error(error);
+    })
+       
+  })
+
+  // var axios = require("axios").default;
+  // const response = await axios.post(
+  //   "https://judge0-extra.p.rapidapi.com/submissions",
+
+  //   {
+  //     headers: {
+  //       "x-rapidapi-host": "judge0-extra.p.rapidapi.com",
+  //       "x-rapidapi-key": "acbeeacb85mshceacaa6063ca021p1f634bjsn6775c6acac73", 
+  //       "content-type": "application/json",
+  //        accept: "application/json",
+  //     },
+  //     data: {
+  //       language_id: 52,
+  //       source_code: 'I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbih2b2lkKSB7CiAgY2hhciBuYW1lWzEwXTsKICBzY2FuZigiJXMiLCBuYW1lKTsKICBwcmludGYoImhlbGxvLCAlc1xuIiwgbmFtZSk7CiAgcmV0dXJuIDA7Cn0=',
+  //       stdin: 'SnVkZ2Uw'
+  //     },
+  //     params: {base64_encoded: 'true', fields: '*'},
+  //   }
+  // );
+
+  // console.log(response);
+}
+
+
+  
+  /*
+  var options = {
+    method: 'GET',
+    url: 'https://judge0-ce.p.rapidapi.com/about',
+    headers: {
+      'x-rapidapi-key': 'acbeeacb85mshceacaa6063ca021p1f634bjsn6775c6acac73',
+      'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+    }
+  };*/
+
+  //request
+  /*
+  const response = await fetch(
+    "https://judge0-extra.p.rapidapi.com/submissions",
+    {
+      method: "POST",
+      headers: {
+        "x-rapidapi-host": "judge0-extra.p.rapidapi.com",
+        "x-rapidapi-key": "Your API Key", //// Get yours for free at https://rapidapi.com/hermanzdosilovic/api/judge0
+        "content-type": "application/json",
+         accept: "application/json",
+      },
+      body: JSON.stringify({
+        source_code: this.state.input,
+        stdin: this.state.user_input,
+        language_id: this.state.language_id,
+      }),
+    }
+  );*/
+
   return (
     <div>
       <div className="nav">
         <div className="nav-buttons">
-          <button className="nav-buttons-run"> Run </button>
+          <button className="nav-buttons-run" onClick={() => handleSubmit()}> Run </button>
           <select className="nav-buttons-choose">
             <option className="nav-buttons-option" value="Python">
               Python
@@ -34,11 +135,6 @@ const IDE = () => {
         </div>
         <div className="nav-logo">
           MUSICAL IDE
-          <img
-            className="nav-logo-img"
-            src="asset/images/logo.svg"
-            alt="logo"
-          />
         </div>
         <div className="nav-music-player">
           <Draggable
@@ -87,6 +183,9 @@ const IDE = () => {
           theme="vs-dark"
           value={code}
           options={options}
+          automaticLayout = {true}
+          onChange ={(value, e) => setCode(value)}
+          // onChange = {(value, e) => handleCodeChange(value, e)}
           // onChange={::this.onChange}
           // editorDidMount={::this.editorDidMount}
         />
@@ -102,7 +201,6 @@ const IDE = () => {
             <textarea
               className="textarea"
               rows="9"
-              cols="80"
               placeholder="Enter input"
             ></textarea>
           </div>
@@ -115,7 +213,6 @@ const IDE = () => {
             <textarea
               className="textarea"
               rows="9"
-              cols="80"
               placeholder="Enter input"
             ></textarea>
           </div>
